7fa6dbbcb1b2c99c415a94b689c2823389f37c32
tom <DevTsai@outlook.com>
2024-04-07 13:36:43 +0000

    test

--- web/front/scripts/check.sh
+++ web/front/scripts/check.sh
@@ -1,1 +1,7 @@
-ls
+output=$(find . -maxdepth 3 | grep -iv -e "^./README.md2" -e "^./web/front/" -e "^./web/front$" -e "^./web$" -e "^./.git/" -e "^./.git$" -e "^./.github/" -e "^./.github$" -e "^./spec/web/front/" -e "^./spec$" -e "^./spec/web$" -e "^./spec/web/front$")
+# assert output is .
+if [ "$output" != "." ]; then
+  echo "unexpected files: $output"
+  echo "exit with 1"
+  exit 1
+fi
