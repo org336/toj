b2328371c51303ba9f240756225a7fcc4463a9b9
tom <DevTsai@outlook.com>
2024-04-07 13:52:39 +0000

    update github action, filter unexpected files

--- web/front/scripts/check.sh
+++ web/front/scripts/check.sh
@@ -1,4 +1,8 @@
-output=$(find . -maxdepth 3 | grep -iv -e "^./README.md2" -e "^./web/front/" -e "^./web/front$" -e "^./web$" -e "^./.git/" -e "^./.git$" -e "^./.github/" -e "^./.github$" -e "^./spec/web/front/" -e "^./spec$" -e "^./spec/web$" -e "^./spec/web/front$")
+output=$(find . -maxdepth 3 | grep -iv -e "^./README.md" \
+-e "^./web/front/" -e "^./web/front$" -e "^./web$" \
+-e "^./.git/" -e "^./.git$" \
+-e "^./.github/" -e "^./.github$" \
+-e "^./spec/web/front/" -e "^./spec$" -e "^./spec/web$" -e "^./spec/web/front$")
 # assert output is .
 if [ "$output" != "." ]; then
   echo "unexpected files: $output"
