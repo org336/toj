d56d84deff6f0860b859d940747878c0b151c6f2
294916437 <du2959346375@outlook.com>
2024-06-10 12:24:33 +0800

    定义了全局拦截器和过滤器，简单实现了连接数据库的注册登录修改密码API，此外配置了swagger文档，在/docs路径下

--- web/back/src/main.ts
+++ web/back/src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ApiInterceptor } from './common/interceptors/api.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+  const options = new DocumentBuilder()
+    .setTitle('后端接口模拟')
+    .setDescription('试着调试看看!')
+    .setVersion('1')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
